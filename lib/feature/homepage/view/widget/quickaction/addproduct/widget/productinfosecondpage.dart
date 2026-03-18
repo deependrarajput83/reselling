a4a83f36d19af15bcc4f6e384305da4b066dd2ec
Deependra Rajput <deependersingh830@gmail.com>
2026-03-18 12:03:04 +0530

    login feature added

--- lib/feature/homepage/view/widget/quickaction/addproduct/widget/productinfosecondpage.dart
+++ lib/feature/homepage/view/widget/quickaction/addproduct/widget/productinfosecondpage.dart
@@ -24,7 +24,7 @@ class ProductInfoSecondPage extends StatelessWidget {
           Commontextformfiled(
             controller: TextEditingController(),
             obscureText: false,
-            hint: "100",
+            hint: "100g",
           ),
           SizedBox(height: AppSize.spacingSm),
           Text(AppTextFile.Tags),
